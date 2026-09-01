0f77f24257b34c58a146286bdabcd94b1e1f9eff
yhxd123 <135325765+yhxd123@users.noreply.github.com>
2026-09-01 15:58:17 +0800

    GDSL: Record Named-field hot-reload result and update repro test

--- gdsl/example/leak_editor.gd
+++ gdsl/example/leak_editor.gd
@@ -2,13 +2,16 @@
 extends Node
 
 static var _p = null
+static var _b = null
 
 func _ready():
 	_p = Player.new()
 	_p.hp = 7
-	print("before reload hp=", _p.hp)
+	_b = Bullet.new()
+	_b.owner = _p
+	print("before reload: hp=", _p.hp, " owner_set=", (_b.owner != null))
 	var gm = Engine.get_singleton("GDExtensionManager")
-	var status = gm.reload_extension("res://self_rule.gdextension")
+	var status = gm.reload_extension("res://named.gdextension")
 	print("reload status=", status)
-	print("after reload hp=", _p.hp)
+	print("after reload: hp=", _p.hp, " owner_same=", (_b.owner == _p))
 	get_tree().quit()
